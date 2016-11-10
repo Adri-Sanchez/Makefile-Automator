fec32265a2c7c6dae7f5fc10ef6aa19ec78f771f
jesusjimsa <jesusjimsa@icloud.com>
2016-11-11 00:50:23 +0100

    First methods

--- include/include_reader.h
+++ include/include_reader.h
@@ -1,5 +1,5 @@
 //
-//  include_reader.hpp
+//  include_reader.h
 //  Makefile Automator
 // 
 //  Created by Jesús Jiménez Sánchez on 7/11/16.
@@ -10,5 +10,39 @@
 #define include_reader_hpp
 
 #include <stdio.h>
+#include <vector>
+#include <string>
 
-#endif /* include_reader_hpp */
+using namespace std;
+
+class Include{
+public:
+	vector<string> user_includes;
+private:
+	/**
+	 @brief Makes a clear object
+	 */
+	Include();
+	
+	/**
+	 @brief Returns a concrete include
+	 @param pos Position in vector
+	 @return Concrete include
+	 */
+	string getUserInclude(int pos);
+	
+	/**
+	 @brief Returns the entire vector
+	 @return The entire vector
+	 */
+	vector<string> getUserInclude();
+	
+	/**
+	 @brief Reads all includes and saves them
+	 
+	 Reads all includes and saves those which are written by the developer
+	 */
+	void read(string complete);
+};
+
+#endif /* include_reader_h */
--- src/include_reader.cpp
+++ src/include_reader.cpp
@@ -6,4 +6,48 @@
 //  Copyright © 2016 Jesús Jiménez Sánchez. All rights reserved.
 //
 
-#include "include_reader.hpp"
+#include "include_reader.h"
+#include <string>
+
+using namespace std;
+
+Include::Include(){
+	user_includes.clear();
+}
+
+string Include::getUserInclude(int pos){
+	string to_return;
+	
+	if(pos < user_includes.size()){
+		to_return = user_includes[pos];
+	}
+	
+	return to_return;
+}
+
+vector<string> Include::getUserInclude(){
+	return user_includes;
+}
+
+void Include::read(string complete){
+	size_t pos_1, pos_2;
+	bool angle, quotation;
+	string looking;
+	
+	angle = false;
+	quotation = false;
+	
+	for(int i = 0; i < complete.length() && !angle && !quotation; i++){
+		if(complete[i] == '<'){
+			angle = true;
+			looking = "<";
+		}
+		
+		if(complete[i] == '"'){
+			quotation = true;
+			looking = "\"";
+		}
+	}
+	
+	//Empezar a leer
+}
